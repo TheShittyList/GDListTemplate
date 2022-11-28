8ed2e6052e268387475a334697598d919fb7dff5
Prometheus <33342399+Prometheus-03@users.noreply.github.com>
2022-11-28 21:01:46 +0800

    Fixed name issues, extended list hard set to 100% qualifying percent

--- js/pages/List.js
+++ js/pages/List.js
@@ -64,7 +64,8 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>
-                    <p v-if="selected + 1 <= 150"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else-if="selected +1 <= 150><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
