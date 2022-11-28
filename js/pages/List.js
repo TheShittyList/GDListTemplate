99a3922af984d0f752d2c035871675c2e030e32a
Prometheus <33342399+Prometheus-03@users.noreply.github.com>
2022-11-28 22:30:45 +0800

    Fixed error

--- js/pages/List.js
+++ js/pages/List.js
@@ -65,7 +65,7 @@ export default {
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
-                    <p v-else-if="selected +1 <= 150><strong>100%</strong> or better to qualify</p>
+                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
