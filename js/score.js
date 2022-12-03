b2e11be2c43a5f27f2ef437de289445f6f584f85
Prometheus <33342399+Prometheus-03@users.noreply.github.com>
2022-12-03 20:59:48 +0800

    New points formula

--- js/score.js
+++ js/score.js
@@ -18,7 +18,13 @@ export function score(rank, percent, minPercent) {
         return 0;
     }
 
+    // Old formula
+    /*
     let score = (100 / Math.sqrt((rank - 1) / 50 + 0.444444) - 50) *
+        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    */
+    // New formula
+    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
